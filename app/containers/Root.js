2d5e89fc32c787de3d902849691c4f955ac2759c
chie_4 <w310930920@gmail.com>
2017-05-24 23:27:02 +0800

    test

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import styles from './Root.css';
+// import styles from './Root.css';
 
-function Repeat(props) {
-  const items = [];
-  for (let i = 0; i < props.numTimes; i++) {
-    items.push(props.children(i));
-  }
-  return <div className={styles.blue}>{items}</div>;
-}
 
 
 export default function App() {
   return (
-    <Repeat numTimes={10}>
-      {(index) => <div key={index}> {index} </div>}
-    </Repeat>
+    <div>
+      10
+    </div>
   );
 }
 
