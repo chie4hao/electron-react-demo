163c9a1c20c20b1c2b2a45c057f164e67e8567f9
chie_4 <w310930920@gmail.com>
2017-05-26 21:07:04 +0800

    sldfk

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 // import styles from './Root.css';
 
+function doge (target) {
+  target.isDoge = "sl";
+}
 
+@doge
+class Dog {}
 
 export default function App() {
   return (
     <div>
-      10
+      {Dog.isDoge}
     </div>
   );
 }
